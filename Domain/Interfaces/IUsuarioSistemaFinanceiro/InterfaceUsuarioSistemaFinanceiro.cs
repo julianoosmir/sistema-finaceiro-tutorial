030c4f314284d514556bfe35b927d9413052e92b
julianoosmir <julianoosmir@outlook.com>
2023-10-25 14:33:11 -0400

    parte 7 comcluidas

---
+++ Domain/Interfaces/IUsuarioSistemaFinanceiro/InterfaceUsuarioSistemaFinanceiro.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Interfaces.Generics;
+using Entities.Entidades;
+
+namespace Domain.Interfaces.IUsuarioSistemaFinanceiro
+{
+    public interface InterfaceUsuarioSistemaFinanceiro : InterfaceGeneric<UsuarioSistemaFinanceiro>
+    {
+        Task<IList<UsuarioSistemaFinanceiro>> ListarUsuariosSistema(int IdSistema);
+
+        Task RemoveUsuarios(List<UsuarioSistemaFinanceiro> usuarios);
+
+        Task<UsuarioSistemaFinanceiro> ObterUsuarioPorEmail(string emailUsuario);
+
+    }
+}
---
+++ Infra/Configuracao/ContextBase.cs
@@ -0,0 +1,46 @@
+﻿using Entities.Entidades;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Infra.Configuracao
+{
+    public class ContextBase : IdentityDbContext<ApplicationUser>
+    {
+        public ContextBase(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<SistemaFinanceiro> SistemaFinanceiro { set; get; }
+        public DbSet<UsuarioSistemaFinanceiro> UsuarioSistemaFinanceiro { set; get; }
+        public DbSet<Categoria> Categoria { set; get; }
+        public DbSet<Despesa> Despesa { set; get; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(ObterStringConexao());
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ApplicationUser>().ToTable("AspNetUsers").HasKey(t => t.Id);
+
+            base.OnModelCreating(builder);
+        }
+
+
+        public string ObterStringConexao()
+        {
+            return "Data Source=NBQSP-FC693;Initial Catalog=FINANCEIRO_2023;Integrated Security=False;User ID=sa;Password=admin123456;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False";
+
+            //return "Data Source=NBQSP-FC693;Initial Catalog=FINANCEIRO_2023;Integrated Security=True"; // Evitar
+        }
+    }
+}
+
